412b10f52033ee1733782b70fa4dcff77c75a881
sunitee <suniteebajpai2@gmail.com>
2021-05-12 18:26:18 -0400

    commit tree 7110b7891bff9400b2d6bc7be675b785c38491a7

--- src/data.js
+++ src/data.js
@@ -1,17 +1,19 @@
+
+
 const movieData = [
-  {
+   { 
     title: 'Choux and Maru go to Istanbul',
     IMDBRating: 3,
     genres: ['cats', 'adventure', 'romance'],
     poster: 'choux-maru-istanbul'
   },
-  {
+  { 
     title: 'Choux and Maru',
     IMDBRating: 4,
     genres: ['cats', 'drama', 'romance'],
     poster: 'choux-maru-part-1'
   },
-  {
+  { 
     title: 'ChromeBoi',
     IMDBRating: 5,
     genres: ['thriller', 'romance'],
@@ -24,31 +26,31 @@ const movieData = [
     genres: ['an awful movie'],
     poster: 'escape-from-vim'
   },
-  {
+  { 
     title: 'GoldenEye',
     IMDBRating: 4,
     genres: ['mystery', 'thriller', 'crime'],
     poster: 'goldeneye'
   },
-  {
+  { 
     title: 'Handsome Boy Modeling Club',
     IMDBRating: 2,
     genres: ['coming of age', 'romance'],
     poster: 'handsome-boy'
   },
-  {
+  { 
     title: "Maru's Spinoff: Trapped in the Sheets",
     IMDBRating: 5,
     genres: ['Adventure'],
     poster: 'marus-spinoff'
   },
-  {
+  { 
     title: 'Terrance: King of the Rats',
     IMDBRating: 1,
     genres: ['drama'],
     poster: 'terrance-king'
   },
-  {
+  { 
     title: 'The Trash Man',
     IMDBRating: 5,
     genres: ['crime', 'period piece'],
@@ -56,4 +58,10 @@ const movieData = [
   },
 ]
 
+
 export default movieData
+
+
+
+
+
